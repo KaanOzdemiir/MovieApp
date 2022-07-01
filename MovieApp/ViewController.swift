de778d97a301aa77b6e51254ae5f4606df3dcd89
KaanOzdemir <kaan.ozdemir@mobillium.com>
2022-07-01 20:36:36 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Kaan Ozdemir on 1.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
